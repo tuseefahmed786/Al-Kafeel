9041cf85ef90b48e1d4c9bcd6d5c99c3fada0dd4
tuseefahmed786 <tuseefahmed8880@gmail.com>
2023-11-22 19:47:43 +0500

    Updates

---
+++ precache-manifest.d69e1b0ac09310085c6ae37d007607bf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ecd9faad819091442c4f9e07b6cc01",
+    "url": "/Al-Kafeel/index.html"
+  },
+  {
+    "revision": "9bc4c90be05040f808f8",
+    "url": "/Al-Kafeel/static/css/main.02d8bc94.chunk.css"
+  },
+  {
+    "revision": "ce8fe68c1ca2d216a483",
+    "url": "/Al-Kafeel/static/js/2.6f5e24cc.chunk.js"
+  },
+  {
+    "revision": "c5e6f2cd6d6c41508f65065d8f7229c4",
+    "url": "/Al-Kafeel/static/js/2.6f5e24cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03083943dd2f9bd2131e",
+    "url": "/Al-Kafeel/static/js/3.65813d43.chunk.js"
+  },
+  {
+    "revision": "9bc4c90be05040f808f8",
+    "url": "/Al-Kafeel/static/js/main.878df134.chunk.js"
+  },
+  {
+    "revision": "22818ea35b9c2651171c",
+    "url": "/Al-Kafeel/static/js/runtime-main.c2a2d241.js"
+  },
+  {
+    "revision": "c16bfe9fa7299f21bbdf78882bcbe37f",
+    "url": "/Al-Kafeel/static/media/bfa2c680692bb91bd4ddbc794618997b.c16bfe9f.jpg"
+  },
+  {
+    "revision": "74c3556b9dad12fb76f84af53ba69410",
+    "url": "/Al-Kafeel/static/media/c9a5bc6a7c948fb0-s.p.74c3556b.woff2"
+  },
+  {
+    "revision": "62b761afc912f35d207e71c43b27695f",
+    "url": "/Al-Kafeel/static/media/docs-right.62b761af.png"
+  },
+  {
+    "revision": "1b2459b992a6af7f67f11779b275866b",
+    "url": "/Al-Kafeel/static/media/empg.1b2459b9.jfif"
+  },
+  {
+    "revision": "3c101cfa233cfe292d6517baf2b228ca",
+    "url": "/Al-Kafeel/static/media/fara.3c101cfa.jfif"
+  },
+  {
+    "revision": "2859822fedd304567794e38a5e4d4c7c",
+    "url": "/Al-Kafeel/static/media/hello.2859822f.mp4"
+  },
+  {
+    "revision": "30b19c44411bf62d653b7c9627801550",
+    "url": "/Al-Kafeel/static/media/me.30b19c44.jfif"
+  },
+  {
+    "revision": "9ac28a2c3a135cf0886690c25ac99f0f",
+    "url": "/Al-Kafeel/static/media/random.9ac28a2c.jfif"
+  },
+  {
+    "revision": "4e9d0d544904593143b99b980e2433d5",
+    "url": "/Al-Kafeel/static/media/rocket.4e9d0d54.jpg"
+  }
+]);
